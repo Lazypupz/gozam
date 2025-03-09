389e820f95ac2cf8fc78e3ca055ebdbfaf920a61
lazypupz++ <antonycasey08@gmail.com>
2025-03-09 19:58:12 +0000

    fym HEADER DOESNT CONTaIN DATA

--- audio/wav.go
+++ audio/wav.go
@@ -1,28 +1,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
+	"math"
 
-	"github.com/go-audio/wav"
+	"github.com/r9y9/gossp"
+	"github.com/r9y9/gossp/io"
+	"github.com/r9y9/gossp/stft"
+	"github.com/r9y9/gossp/window"
 )
 
-func wavSpec() {
-	f, err := os.Open("../wav/bib.wav")
+/*
+	 func wavSpec() {
+		f, err := os.Open("../wav/bib.wav")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		d := wav.NewDecoder(f)
+		if !d.IsValidFile() {
+			log.Fatal("get scammed buddy")
+		}
+		fmt.Println(d.SampleRate)
+		fmt.Println(d.SampleBitDepth())
+
+}
+*/
+func createSpec() {
+	test_recording := flag.String("i", "../wav/fixed.wav", "fixed.wav")
+	flag.Parse()
+
+	w, err := io.ReadWav(*test_recording)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
-	d := wav.NewDecoder(f)
-	if !d.IsValidFile() {
-		log.Fatal("get scammed buddy")
-	}
-	fmt.Println(d.SampleRate)
-	fmt.Println(d.SampleBitDepth())
 
+	data := w.GetMonoData()
+	s := &stft.STFT{
+		FrameShift: int(float64(w.SampleRate) / 100.0),
+		FrameLen:   2048,
+		Window:     window.CreateHanning(2048),
+	}
+	spectrogram, _ := gossp.SplitSpectrogram(s.STFT(data))
+	PrintMatrixAsGnuplotFormat(spectrogram)
+}
+
+func PrintMatrixAsGnuplotFormat(matrix [][]float64) {
+	fmt.Println("#", len(matrix[0]), len(matrix)/2)
+	for i, vec := range matrix {
+		for j, val := range vec[:1024] {
+			fmt.Println(i, j, math.Log(val))
+		}
+		fmt.Println("")
+	}
+}
+
+func tft() {
+	t := flag.String("i", "../wav/Recording.wav", "Recording")
+	flag.Parse()
+
+	fmt.Println("FilePath:", *t)
+	//test file path
 }
 
 func main() {
-	wavSpec()
+	createSpec()
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,12 @@ go 1.24.1
 require github.com/go-audio/wav v1.1.0
 
 require (
+	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
+	github.com/r9y9/go-dsp v0.0.0-20140323125827-6ccdfeca7723 // indirect
+)
+
+require (
 	github.com/go-audio/audio v1.0.0 // indirect
 	github.com/go-audio/riff v1.0.0 // indirect
+	github.com/r9y9/gossp v0.0.1
 )
